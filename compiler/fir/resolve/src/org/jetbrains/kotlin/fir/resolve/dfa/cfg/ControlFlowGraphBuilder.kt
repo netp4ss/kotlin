c0c920dc0f8a6d182ed6ed757d7d6f3066fed848
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-07-01 16:43:35 +0200

    Add FirScript element and appropriate infrastructure

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/cfg/ControlFlowGraphBuilder.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/cfg/ControlFlowGraphBuilder.kt
@@ -36,6 +36,9 @@ class ControlFlowGraphBuilder {
     val lastNode: CFGNode<*>
         get() = lastNodes.top()
 
+    val lastNodeOrNull: CFGNode<*>?
+        get() = lastNodes.topOrNull()
+
     var levelCounter: Int = 0
         private set
 
@@ -160,7 +163,7 @@ class ControlFlowGraphBuilder {
         )
 
         val previousNode = enterToLocalClassesMembers[function.symbol]
-            ?: (function as? FirSimpleFunction)?.takeIf { it.isLocal }?.let { lastNode }
+            ?: (function as? FirSimpleFunction)?.takeIf { it.isLocal }?.let { lastNodeOrNull }
 
         val enterNode = createFunctionEnterNode(function).also {
             lastNodes.push(it)
@@ -1043,9 +1046,11 @@ class ControlFlowGraphBuilder {
         node: CFGNode<*>,
         isDead: Boolean = false,
         preferredKind: EdgeKind = EdgeKind.Forward
-    ): CFGNode<*> {
-        val lastNode = lastNodes.pop()
-        addEdge(lastNode, node, isDead = isDead, preferredKind = preferredKind)
+    ): CFGNode<*>? {
+        val lastNode = lastNodes.popOrNull()
+        if (lastNode != null) {
+            addEdge(lastNode, node, isDead = isDead, preferredKind = preferredKind)
+        }
         lastNodes.push(node)
         return lastNode
     }
